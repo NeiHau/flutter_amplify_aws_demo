75ddef2ecd3535e676dc4ff061e68f8131111ce4
kaito.uchida <kaito.uchida@stv-tech.co.jp>
2023-07-16 21:55:15 +0900

    introduce RxDart logic

--- lib/fearures/crypto/presentation/crypto_page.dart
+++ lib/fearures/crypto/presentation/crypto_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../utils/in_memory_store.dart';
 import '../client/crypto_client.dart';
 
 class CryptoScreen extends ConsumerWidget {
@@ -10,6 +11,7 @@ class CryptoScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final priceUSDAsyncValue = ref.watch(bitcoinPriceUSDProvider);
     final priceJPYAsyncValue = ref.watch(bitcoinPriceJPYProvider);
+    final previousPriceUSD = ref.watch(previousPriceUSDProvider.notifier);
 
     return Scaffold(
       body: Center(
@@ -17,10 +19,26 @@ class CryptoScreen extends ConsumerWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             priceUSDAsyncValue.when(
-              data: (priceUSD) => Text(
-                'Bitcoin Price: $priceUSD USD',
-                style: const TextStyle(fontSize: 24),
-              ),
+              data: (priceUSD) {
+                // Compare the current price with the last price
+                final iconUSD = (previousPriceUSD.state.value ?? 0.0) > priceUSD
+                    ? Icons.arrow_downward
+                    : Icons.arrow_upward;
+
+                // Update the last price
+                previousPriceUSD.state.value = priceUSD;
+
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Bitcoin Price: $priceUSD USD',
+                      style: const TextStyle(fontSize: 24),
+                    ),
+                    Icon(iconUSD, color: Colors.red), // Add the arrow icon
+                  ],
+                );
+              },
               loading: () => const CircularProgressIndicator(),
               error: (_, __) =>
                   const Text('Failed to load bitcoin price in USD.'),
---
+++ lib/utils/in_memory_store.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rxdart/rxdart.dart';
+
+final previousPriceUSDProvider =
+    StateProvider<InMemoryStore<double>>((_) => InMemoryStore<double>(0.0));
+
+class InMemoryStore<T> {
+  InMemoryStore(T initial) : _subject = BehaviorSubject<T>.seeded(initial);
+
+  final BehaviorSubject<T?> _subject;
+
+  Stream<T?> get stream => _subject.stream;
+
+  T? get value => _subject.value;
+
+  set value(T? value) {
+    _subject.sink.add(value);
+  }
+
+  void close() => _subject.close();
+}
